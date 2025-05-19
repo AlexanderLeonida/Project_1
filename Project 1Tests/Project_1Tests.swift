b4a1d85ad5f6156ab8bc3af6ea1e75e5f48f5ed3
Alexander Leonida <alexander.leonida@whoweplayfor.org>
2025-05-19 11:38:55 -0700

    initial commit

---
+++ Project 1Tests/Project_1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Project_1Tests.swift
+//  Project 1Tests
+//
+//  Created by Alex Leonida on 5/19/25.
+//
+
+import Testing
+
+struct Project_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
